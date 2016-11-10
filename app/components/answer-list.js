698a5ea8832cea9eb4f189956ef368f136d703fe
Rebecca Allen <rebeccazarsky@gmail.com>
2016-11-10 18:41:47 -0500

    add computed property

--- app/components/answer-list.js
+++ app/components/answer-list.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  fullAnswer: Ember.computed('answer.answer', 'answer.author', function() {
+    return this.get('answer.answer') + ' - ' + this.get('answer.author');
+  }),
   actions: {
     delete(answer) {
       if(confirm('Are you sure you want to delete this answer?')) {
