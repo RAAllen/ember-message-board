25ea4f9f9367943e4c07141baffd758a19372170
Rebecca Allen <rebeccazarsky@gmail.com>
2016-11-13 13:54:03 -0500

    add helper

---
+++ tests/unit/helpers/popular-question-test.js
@@ -0,0 +1,12 @@
+
+import { popularQuestion } from 'ember-message-board/helpers/popular-question';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | popular question');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = popularQuestion([42]);
+  assert.ok(result);
+});
+
