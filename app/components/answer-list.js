5cb8eee444858e12be9c1854682932ddf55bf38a
Rebecca Allen <rebeccazarsky@gmail.com>
2016-11-12 19:33:03 -0500

    styling

--- app/components/answer-list.js
+++ app/components/answer-list.js
@@ -1,14 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  selectedAnswers: Ember.inject.service(),
   fullAnswer: Ember.computed('answer.answer', 'answer.author', function() {
     return this.get('answer.answer') + ' - ' + this.get('answer.author');
   }),
   actions: {
-    addToSelectedAnswersList(answer) {
-      this.get('selectedAnswers').add(answer);
-    }
     delete(answer) {
       if(confirm('Are you sure you want to delete this answer?')) {
         this.sendAction('destroyAnswer', answer);
--- app/services/selected-answer-list.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Service.extend({
-  selectedAnswers: [],
-
-  add(answer) {
-    this.get('selectedAnswers').pushObject(answer);
-  }
-});
